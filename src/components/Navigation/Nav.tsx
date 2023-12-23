bbbffdaa062b54ef433f900bef71ffb8c3067e0c
Joschka Eickhoff <jeickhoff@posteo.de>
2023-12-23 03:17:27 +0100

    added nav

--- src/components/Navigation/Nav.tsx
+++ src/components/Navigation/Nav.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import navCategories from "../../utils/navCategories.json";
-import styles from "./Component.module.scss";
+import styles from "./Nav.module.scss";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
   const clickHandler = (e: React.MouseEvent<HTMLLIElement>) => {
     console.log(e.currentTarget);
-    console.log("ad");
   };
 
   return (
     <ul className={styles.ul}>
       {navCategories.map((item) => (
-        <li className={styles.li} key={item.name} onClick={clickHandler}>
-          {item.name}
+        <li className={styles.li} key={item.id} onClick={clickHandler}>
+          <Link className={styles.a} to={item.url}>
+            {item.name}
+            <span className={styles.line} />
+          </Link>
         </li>
       ))}
     </ul>
